5188fd56fe03497cf6aced72cdbb7107b1c2ae0d
Max C <smashmouthmax@gmail.com>
2023-01-14 19:25:55 -0800

    added delete button -- need to implement delete functionality

--- script.js
+++ script.js
@@ -110,6 +110,7 @@ class App {
   }
 
   _getPosition() {
+    //get user position
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         this._loadMap.bind(this),
@@ -442,14 +443,26 @@ class App {
     <li class="workout workout--${workout.type}" data-id="${workout.id}">
           <h2 class="workout__title">
             ${workout.description}
-            <button type="button" class="workout__edit workout__edit__button">
-              <svg xmlns="http://www.w3.org/2000/svg" class="icon icon__edit" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                <path d="M9 7h-3a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-3" />
-                <path d="M9 15h3l8.5 -8.5a1.5 1.5 0 0 0 -3 -3l-8.5 8.5v3" />
-                <line x1="16" y1="5" x2="19" y2="8" />
-              </svg>
-            </button>
+            <div class="workout__button__container">
+              <button type="button" class="workout__trash workout__trash__button">
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon__trash" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                  <line x1="4" y1="7" x2="20" y2="7" />
+                  <line x1="10" y1="11" x2="10" y2="17" />
+                  <line x1="14" y1="11" x2="14" y2="17" />
+                  <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+                  <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+                </svg>
+              </button>
+              <button type="button" class="workout__edit workout__edit__button">
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon__edit" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                  <path d="M9 7h-3a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-3" />
+                  <path d="M9 15h3l8.5 -8.5a1.5 1.5 0 0 0 -3 -3l-8.5 8.5v3" />
+                  <line x1="16" y1="5" x2="19" y2="8" />
+                </svg>
+              </button>
+            </div>
           </h2>
           <div class="workout__details">
             <span class="workout__icon">${
